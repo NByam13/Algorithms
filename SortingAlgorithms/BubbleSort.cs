e70af320960dca6c61ae791e9b5ff123640c2eb3
Tangent1al <y.dydxx.b@gmail.com>
2021-01-25 17:03:53 -0500

    Bubble sort added, still needs overloaded methods added for different data types

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Sort_Algorithms
+{
+    public static class BubbleSort
+    {
+        public static List<int> SortListInt(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[i] < list[j]) // This will ensure the values are listed in ascending order
+                    {
+                        int temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
